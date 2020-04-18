a67219b9d4e46a5459fe41c5308fad8d96b5202f
kkBill <mjy332528@163.com>
2020-04-18 23:41:02 +0800

    day2, concurrent lru cache

---
+++ day2/src/gomemcache_test.go
@@ -0,0 +1,65 @@
+package gomemcache
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+	"testing"
+)
+
+func TestGetter(t *testing.T)  {
+	var f Getter = GetterFunc(func(key string) ([]byte, error) {
+		return []byte(key), nil
+	})
+
+	expect := []byte("key")
+	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
+		t.Fatal("callback failed.")
+	}
+}
+
+// 利用map简单的模拟数据流
+var db = map[string]string {
+	"Tom":"630",
+	"Jack":"589",
+	"Sam":"567",
+}
+
+func TestGet(t *testing.T) {
+	loadCounts := make(map[string]int, len(db))
+
+	// 创建缓存实例，并指定回调函数
+	cache := NewGroup("scores", 1024, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Printf("search key[%s] in db.\n", key)
+			if v, ok := db[key]; ok {
+				if _, ok := loadCounts[key]; !ok {
+					loadCounts[key] = 0
+				}
+				loadCounts[key] += 1
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("key[%s] does not exist.\n", key)
+		}))
+
+	// 模拟查询
+	for k, v := range db {
+		//
+		if res, err := cache.Get(k); err != nil || res.String() != v {
+			t.Errorf("fail to get value of %s.\n", k)
+		}
+		//
+		if _, err := cache.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Errorf("key[%s] missed in cache.\n", k)
+		}
+	}
+
+	if res, err := cache.Get("unknown"); err == nil {
+		t.Errorf("the value of key[unknown] should be empty, but [%s] got.\n", res)
+	}
+}
+
+
+func hash(key int, nodes int) int {
+	return key % nodes
+}
